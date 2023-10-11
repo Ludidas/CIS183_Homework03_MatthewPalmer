30b46d259911061150e02425f627d94a70fabb76
Ludidas <skyblastbomber@gmail.com>
2023-10-11 14:09:21 -0400

    Small Changes

--- app/src/main/java/com/example/homework03_program01/MainActivity.java
+++ app/src/main/java/com/example/homework03_program01/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.homework03_program01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button ma_btn_j_addUser;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ma_btn_j_addUser=findViewById(R.id.ma_btn_v_addUser);
+
+        //changeButtonColor();
+    }
+
+    public void changeButtonColor()
+    {
+        ma_btn_j_addUser.setTextColor(0xFFFAAA);
     }
 }
